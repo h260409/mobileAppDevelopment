e60ca4e21fa2b9f62c27088dca5f4486290cb484
Gyulai-Nagy Tamas <h260409@stud.u-szeged.hu>
2025-04-14 01:03:06 +0200

    version fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application")
-    id("com.google.gms.google-services")
+    id("com.android.application") version "8.8.1"
+    id("com.google.gms.google-services") version "4.4.2"
 }
 
 android {
